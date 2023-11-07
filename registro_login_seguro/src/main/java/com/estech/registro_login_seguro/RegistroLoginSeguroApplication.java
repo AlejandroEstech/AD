075768038c784d63a315dd461d9fe4b76a9f161c
AlejandroEstech <alejandro.munoz@escuelaestech.es>
2023-11-07 12:58:39 +0100

    Acceso a Datos

---
+++ registro_login_seguro/src/main/java/com/estech/registro_login_seguro/RegistroLoginSeguroApplication.java
@@ -0,0 +1,13 @@
+package com.estech.registro_login_seguro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroLoginSeguroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroLoginSeguroApplication.class, args);
+	}
+
+}
---
+++ registro_login_seguro/src/test/java/com/estech/registro_login_seguro/RegistroLoginSeguroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estech.registro_login_seguro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroLoginSeguroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
